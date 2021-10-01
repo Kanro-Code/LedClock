c914132f89931d2884b18911055726aea988b9a8
Kanro <kanro.wow@gmail.com>
2021-10-01 05:24:00 +0200

    Cleanup

--- src/LedClock.cpp
+++ src/LedClock.cpp
@@ -1,23 +1,25 @@
 #include <Arduino.h>
-#define FASTLED_ALLOW_INTERRUPTS 0
 #include <FastLED.h>
+#define FASTLED_ALLOW_INTERRUPTS 0
 
 byte getPos(byte pos);
 byte mapMinMax(byte v, byte oldMin, byte oldMax, byte newMin, byte newMax);
 
-#define LED_COUNT 83
 #define ROWS 11
 #define COLS 7
-#define BOTTOMLEFT true
-#define CUT_CORNER true
+// Sets if the LED strips start at the bottom-left, or top-left.
+#define STARTBOTTOM true
+// Sets how many deadpixels there are at the top of the LEDSTRIP in the turn.
+#define DEADPIXELS 2
 #define LED_PIN 6
+#define LEDCOUNT ROWS *COLS + COLS / 2 * DEADPIXELS
 
 byte coords[COLS][ROWS];
-CRGB leds[LED_COUNT];
+CRGB leds[LEDCOUNT];
 
 void setup() {
   Serial.begin(57600);
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, LED_COUNT);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, LEDCOUNT);
 
   byte led = 0;
   for (byte x = 0; x < COLS; x++) {
@@ -26,19 +28,13 @@ void setup() {
     }
   }
 
-  for (byte x = (BOTTOMLEFT == true) ? 0 : 1; x < COLS; x = x + 2) {
+  for (byte x = (STARTBOTTOM == true) ? 0 : 1; x < COLS; x = x + 2) {
     for (byte y = 0; y < ROWS / 2; y++) {
       byte eq = mapMinMax(y, 0, ROWS - 1, ROWS - 1, 0);
       byte swap = coords[x][eq];
 
       coords[x][eq] = coords[x][y];
       coords[x][y] = swap;
-    }
-  }
-
-  for (int x = 0; x < COLS; x++) {
-    for (int y = 0; y < ROWS; y++) {
-      Serial.println(coords[x][y]);
     }
   }
 }
@@ -48,7 +44,7 @@ void loop() {
   for (byte x = 0; x < COLS; x++) {
     for (byte y = 0; y < ROWS; y++) {
       FastLED.clear();
-      leds[coords[x][y]] = CRGB::Red;
+      leds[coords[x][y]] = CHSV(random(0,255),random(0,255),random(0,255));
       FastLED.show();
       delay(10);
     }
@@ -56,11 +52,8 @@ void loop() {
 }
 
 byte getPos(byte pos) {
-  if (CUT_CORNER == true) {
-    return pos + floor((pos + ROWS) / (ROWS * 2)) * 2;
-  } else {
-    return pos;
-  }
+  return (DEADPIXELS > 0) ? pos + floor((pos + ROWS) / (ROWS * 2)) * DEADPIXELS
+                          : pos;
 }
 
 byte mapMinMax(byte v, byte oMin, byte oMax, byte nMin, byte nMax) {
