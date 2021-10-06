8b9e53d9b01fe139972e79987ebc4e6b253d6e71
Kanro <kanro.wow@gmail.com>
2021-10-06 07:27:59 +0200

    working fetcher of time, start of serial epoch time setter

--- src/LedClock.cpp
+++ src/LedClock.cpp
@@ -19,6 +19,7 @@ void drawNumber(byte number, byte x, byte y);
 
 byte coords[COLS][ROWS];
 CRGB leds[LEDCOUNT];
+Timer timer;
 
 void setup() {
   Serial.begin(57600);
@@ -40,25 +41,32 @@ void setup() {
       coords[x][y] = swap;
     }
   }
+  timer.init();
 }
 
 byte k = 0;
 void loop() {
-  for (byte x = 0; x < COLS; x++) {
-    for (byte y = 0; y < ROWS; y++) {
-      FastLED.clear();
-      // leds[coords[x][y]] = CHSV(random(0, 255), random(0, 255), random(0, 255));
-      drawNumber(random(0, 9), 0, 0);
-      drawNumber(random(0, 9), 4, 0);
-      drawNumber(random(0, 9), 0, 6);
-      drawNumber(random(0, 9), 4, 6);
-      k++;
-      if (k >= 10)
-        k = 0;
-      FastLED.show();
-      delay(1000);
-    }
+  if (Serial.available()) {
+    timer.getSerial();
   }
+  Serial.println("ping");
+  timer.getTime();
+  delay(1000);
+  // for (byte x = 0; x < COLS; x++) {
+  //   for (byte y = 0; y < ROWS; y++) {
+  //     FastLED.clear();
+  //     // leds[coords[x][y]] = CHSV(random(0, 255), random(0, 255), random(0, 255));
+  //     drawNumber(random(0, 9), 0, 0);
+  //     drawNumber(random(0, 9), 4, 0);
+  //     drawNumber(random(0, 9), 0, 6);
+  //     drawNumber(random(0, 9), 4, 6);
+  //     k++;
+  //     if (k >= 10)
+  //       k = 0;
+  //     FastLED.show();
+  //     delay(1000);
+  //   }
+  // }
 }
 
 byte getPos(byte pos) {
--- src/TImer.h
+++ src/TImer.h
@@ -1,25 +1,55 @@
+#include "RTClib.h"
+RTC_DS3231 rtc;
 
-class Timer {
-  public:
-    static void getTime();
-    static void setTime(int t);
-    static void getSerial();
-  private:
-    int refreshed;
-    int t;
+class Timer
+{
+public:
+  static void init();
+  static void getTime();
+  static void setTime(int t);
+  static void getSerial();
+
+private:
+  int refreshed;
+  int t;
 };
 
-void Timer::getTime() {
-  Serial.println("Hi");
-  Serial.println("there");
+void Timer::getTime()
+{
+  DateTime now = rtc.now();
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
 };
 
-void Timer::setTime(int t) {
-
+void Timer::init()
+{
+  if (!rtc.begin())
+  {
+    Serial.println("Something is wrong with the RTC module.");
+    Serial.flush();
+    abort();
+  }
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+  }
 }
 
-void Timer::getSerial() {
-  while (Serial.available() > 0) {
+void Timer::setTime(int t)
+{
+}
 
-  }
+void Timer::getSerial()
+{
+  String s = Serial.readString();
+  Serial.println("Received:" + s);
 }
